0c1b8eeecd1b0fb2a0e7098c1eb3a0ed2381f5e3
Sera Lee <ssellu.lee@gmail.com>
2021-05-17 17:57:18 +0900

    Refactor Localization

--- src/main/java/com/ssellu/tripmanager/config/WebMvcConfig.java
+++ src/main/java/com/ssellu/tripmanager/config/WebMvcConfig.java
@@ -27,7 +27,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Bean
     public LocaleResolver localeResolver() {
         SessionLocaleResolver slr = new SessionLocaleResolver();
-        slr.setDefaultLocale(Locale.US);
+        slr.setDefaultLocale(Locale.KOREA);
         return slr;
     }
 
