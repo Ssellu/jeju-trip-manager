c409939dc3c21007cc6795d292ad67e3a2b1061c
Sera Lee <ssellu.lee@gmail.com>
2021-05-17 15:16:03 +0900

    Init Project

---
+++ src/main/java/com/ssellu/tripmanager/JejuTripManagerApplication.java
@@ -0,0 +1,13 @@
+package com.ssellu.tripmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JejuTripManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JejuTripManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ssellu/tripmanager/JejuTripManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssellu.tripmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JejuTripManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
