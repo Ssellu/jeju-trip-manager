99bd822ce213d2a8590ad1337aeddc779eb6eac6
Sera Lee <ssellu.lee@gmail.com>
2021-05-17 16:35:09 +0900

    Locale Setting

---
+++ src/main/java/com/ssellu/tripmanager/config/WebSecurityConfig.java
@@ -0,0 +1,25 @@
+package com.ssellu.tripmanager.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        super.configure(web);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/spiters/me").authenticated()
+                .antMatchers(HttpMethod.POST,".spittles").authenticated()
+                .anyRequest().permitAll();
+    }
+}
