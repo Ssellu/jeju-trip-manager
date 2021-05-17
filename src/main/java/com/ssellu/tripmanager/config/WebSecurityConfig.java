e55669a707ae34e6a5eb628372a59e72f2ced1d5
Sera Lee <ssellu.lee@gmail.com>
2021-05-17 17:58:29 +0900

    Add Bootstrap, Jquery, JDenticons and Font-Awesome

--- src/main/java/com/ssellu/tripmanager/config/WebSecurityConfig.java
+++ src/main/java/com/ssellu/tripmanager/config/WebSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.ssellu.tripmanager.config;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -11,7 +12,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     public void configure(WebSecurity web) throws Exception {
-        super.configure(web);
+        web.ignoring()
+                .mvcMatchers("/node_modules/**")
+                .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
     }
 
     @Override
